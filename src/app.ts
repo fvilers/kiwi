f6974e2c90a7edb0bc31ffac5b77a7bc5b5c049d
Fabian Vilers <fabian.vilers@dev-one.com>
2022-06-20 11:39:52 +0200

    Add the morgan logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import logger from "morgan";
 import path from "node:path";
 import { errorHandler } from "./middlewares/error-handler";
 import { mapUrlToLocalFile } from "./middlewares/map-url-to-local-file";
@@ -7,6 +8,7 @@ import { notFound } from "./middlewares/not-found";
 
 export const app = express();
 
+app.use(logger("dev"));
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(mapUrlToLocalFile());
 app.use(markdown());
